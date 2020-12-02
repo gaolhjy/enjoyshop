8388b1d9b33f8d067082ca8e0812a9f83604953e
gaolhjy <984992087@qq.com>
2020-12-02 09:59:00 +0800

    适配Android10 版本

--- okhttputils/src/main/java/com/zhy/http/okhttp/callback/IGenericsSerializator.java
+++
@@ -1,8 +0,0 @@
-package com.zhy.http.okhttp.callback;
-
-/**
- * Created by JimGong on 2016/6/23.
- */
-public interface IGenericsSerializator {
-    <T> T transform(String response, Class<T> classOfT);
-}
